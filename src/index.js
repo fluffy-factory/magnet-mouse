cd100af3745e3be561abd4c7c2cb59e521a8d5fe
Guillaume Egloff <guillaume@plugandcom.com>
2019-07-09 15:01:26 +0200

    Keywords package.json + callback function

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ export default class MagnetMouse {
       magnet: {
         element: '.magnet-mouse',
         class: 'magnet-mouse-active',
-        active: true,
+        enabled: true,
         distance: 20,
         position: 'center'
       },
@@ -14,7 +14,9 @@ export default class MagnetMouse {
         element: '.follow-mouse',
         class: 'follow-mouse-active'
       },
-      throttle: 10
+      throttle: 10,
+      inCallback: null,
+      outCallback: null,
     };
 
     function isMergeableObject(val) {
@@ -36,7 +38,7 @@ export default class MagnetMouse {
 
     function defaultArrayMerge(target, source, optionsArgument) {
       let destination = target.slice();
-      source.forEach(function(e, i) {
+      source.forEach(function (e, i) {
         if (typeof destination[i] === 'undefined') {
           destination[i] = cloneIfNecessary(e, optionsArgument)
         } else if (isMergeableObject(e)) {
@@ -67,7 +69,7 @@ export default class MagnetMouse {
 
     function deepmerge(target, source, optionsArgument) {
       let array = Array.isArray(source);
-      let options = optionsArgument || { arrayMerge: defaultArrayMerge };
+      let options = optionsArgument || {arrayMerge: defaultArrayMerge};
       let arrayMerge = options.arrayMerge || defaultArrayMerge;
 
       if (array) {
@@ -188,26 +190,52 @@ export default class MagnetMouse {
             y = posMouse.y - (data.yMin + $this.config.magnet.distance + data.elem.height / 2);
         }
 
-        data.elem.node.style.transform = 'translate3d(' + x + 'px,' + y + 'px, 0)';
-        data.elem.node.classList.add($this.config.magnet.class);
-
         if ($this.elementFollow.length > 0) {
           $this.elementFollow.forEach(function (element) {
             element.classList.add($this.config.follow.class);
           });
         }
 
+        // Callback when mouse enter in element else add class
+        if ($this.config.inCallback !== null && typeof $this.config.inCallback === 'function') {
+
+          $this.config.inCallback.call(this, data);
+
+        } else {
+
+          // Move element accordiing the mouse
+          data.elem.node.style.transform = 'translate3d(' + x + 'px,' + y + 'px, 0)';
+          data.elem.node.classList.add($this.config.magnet.class);
+
+        }
       } else {
-        data.elem.node.classList.remove($this.config.magnet.class);
-        data.elem.node.style.transform = '';
 
         if ($this.elementFollow.length > 0) {
           $this.elementFollow.forEach(function (element) {
             element.classList.remove($this.config.follow.class);
           });
         }
+
+        // Callback when mouse leave in element else remove class
+        if ($this.config.outCallback !== null && typeof $this.config.outCallback === 'function') {
+
+          $this.config.outCallback.call(this, data);
+
+        } else {
+
+          // Places the element in the initial position
+          data.elem.node.classList.remove($this.config.magnet.class);
+          data.elem.node.style.transform = '';
+
+        }
       }
     });
+  };
+
+  static mobileAndTabletcheck() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
   };
 
   // Add class to each element when the mouse enter in their zone
@@ -224,39 +252,41 @@ export default class MagnetMouse {
   };
 
   init() {
-    let posMouse, posElement, $this = this;
+    let posMouse, posElement, $this = this, mobileTablet = MagnetMouse.mobileAndTabletcheck();
 
-    // On resize, calculate position of element
-    this.resizeFunction = MagnetMouse.throttle(() => {
-      posElement = $this.getPositionElement();
-    }, $this.config.throttle);
+    if (!mobileTablet) {
+      // On resize, calculate position of element
+      this.resizeFunction = MagnetMouse.throttle(() => {
+        posElement = $this.getPositionElement();
+      }, $this.config.throttle);
 
-    // On mouse move, magnet element to the mouse or just hover function
-    this.mouseFunction = MagnetMouse.throttle((e) => {
-      posMouse = MagnetMouse.getPositionMouse(e);
+      // On mouse move, magnet element to the mouse or just hover function
+      this.mouseFunction = MagnetMouse.throttle((e) => {
+        posMouse = MagnetMouse.getPositionMouse(e);
 
-      if ($this.config.magnet.active) {
-        $this.magnetElement(posElement, posMouse);
-      } else {
-        $this.hoverElement(posElement, posMouse);
-      }
+        if ($this.config.magnet.enabled) {
+          $this.magnetElement(posElement, posMouse);
+        } else {
+          $this.hoverElement(posElement, posMouse);
+        }
 
-      if (this.elementFollow.length > 0) {
-        this.elementFollow.forEach(function (element) {
-          element.style.transform = 'translate3d(' + posMouse.x + 'px,' + posMouse.y + 'px, 0)';
-        });
-      }
+        if (this.elementFollow.length > 0) {
+          this.elementFollow.forEach(function (element) {
+            element.style.transform = 'translate3d(' + posMouse.x + 'px,' + posMouse.y + 'px, 0)';
+          });
+        }
 
-    }, $this.config.throttle);
+      }, $this.config.throttle);
 
-    window.addEventListener('resize', this.resizeFunction);
+      window.addEventListener('resize', this.resizeFunction);
 
-    // Calculate position of element when page load
-    document.addEventListener('DOMContentLoaded', function () {
-      posElement = $this.getPositionElement();
-    });
+      // Calculate position of element when page load
+      document.addEventListener('DOMContentLoaded', function () {
+        posElement = $this.getPositionElement();
+      });
 
-    window.addEventListener('mousemove', this.mouseFunction);
+      window.addEventListener('mousemove', this.mouseFunction);
+    }
   }
 
   destroy() {
