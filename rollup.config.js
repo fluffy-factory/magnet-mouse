625088a2c66884ca0cdd437fb4785816212415eb
Guillaume Egloff <guillaume@plugandcom.com>
2019-07-09 11:44:23 +0200

    Package main/module

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
